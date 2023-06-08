0cd738aae6541ef417069b414229c53a6de7f60e
Suhani166 <suhani.jain@learner.manipal.edu>
2023-06-08 14:50:27 +0530

    crypto commit

---
+++ src/Component/Convertor.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { Card, Form, Input, Select } from "antd";
+import { SiBitcoinsv } from "react-icons/si";
+
+function Convertor() {
+	const apiUrl = "https://api.coingecko.com/api/v3/exchange_rates";
+
+	const defaultFirstSelectValue = "Bitcoin";
+	const defaultSecondSelectValue = "Ether";
+
+	const [cryptoList, setCryptoList] = useState([]);
+	const [inputValue, setInputValue] = useState("0");
+	const [firstSelect, setFirstSelect] = useState(defaultFirstSelectValue);
+	const [secondSelect, setSecondSelect] = useState(defaultSecondSelectValue);
+	const [result, setResult] = useState("0");
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	async function fetchData() {
+		const response = await fetch(apiUrl);
+		const JsonData = await response.json();
+
+		const data = JsonData.rates;
+		const tempArray = Object.entries(data).map((item) => {
+			return {
+				value: item[1].name,
+				label: item[1].name,
+				rate: item[1].value,
+			};
+		});
+
+		setCryptoList(tempArray);
+	}
+
+	useEffect(
+		() => {
+			if (cryptoList.length == 0) return;
+
+			const firstSelectRate = cryptoList.find((item) => {
+				return item.value === firstSelect;
+			}).rate;
+
+			const secondSelectRate = cryptoList.find((item) => {
+				return item.value === secondSelect;
+			}).rate;
+
+			const resultValue =
+				(inputValue * secondSelectRate) / firstSelectRate;
+			setResult(resultValue.toFixed(6));
+		},
+		[inputValue],
+		[firstSelect],
+		[secondSelect]
+	);
+
+	return (
+		<div className="container">
+			<Card
+				className="crypto-card"
+				title={
+					<h1>
+						{" "}
+						<SiBitcoinsv /> Crypto-convertor
+					</h1>
+				}
+			>
+				<Form size="large">
+					<Form.Item>
+						<Input
+							onChange={(event) =>
+								setInputValue(event.target.value)
+							}
+						></Input>
+					</Form.Item>
+				</Form>
+				<div className="select-container">
+					<Select
+						style={{ width: "200px" }}
+						defaultValue={defaultFirstSelectValue}
+						options={cryptoList}
+						onChange={(value) => setFirstSelect(value)}
+					/>
+					<Select
+						style={{ width: "200px" }}
+						defaultValue={defaultSecondSelectValue}
+						options={cryptoList}
+						onChange={(value) => setSecondSelect(value)}
+					/>
+				</div>
+				<p className="converted-result">
+					{inputValue} {firstSelect} = {result} {secondSelect}
+				</p>
+			</Card>
+		</div>
+	);
+}
+
+export default Convertor;
